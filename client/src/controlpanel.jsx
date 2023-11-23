504118d5163e0b1ad9607fd182aa2982a119110e
Keizaburo Takase <tkzrsf@gmail.com>
2023-11-24 01:38:48 +0900

    recreate control panel table using mui

--- client/src/controlpanel.jsx
+++ client/src/controlpanel.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
+import { Table, TableHead, TableRow, TableCell, TableBody, TableContainer } from '@mui/material';
+import { Checkbox } from '@mui/material';
 import './controlpanel.css';
+import { Tab } from '@mui/material';
 
 export const ControlPanel = (props) => {
     return (
         <div className="control-panel">
-            <table>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Max Altitude</th>
-                        <th>Duration</th>
-                    </tr>
-                </thead>
-                <tbody>{
-                    props.tracks.map((track, i) => {
-                        const checked = track.show ? "checked" : "";
-                        return (
-                            <tr key={"tr" + i}>
-                                <td key={"show-track-td" + i}>
-                                    <input type="checkbox" className="show-track" onChange={() => { props.onChange(track.id) }} key={"show-track" + i} checked={checked} />
-                                </td>
-                                <td key={"track-color-td" + i}>
-                                    <div className="track-color" key={"track-color" + i} style={{ backgroundColor: track.color.toCssHexString() }}>　</div>
-                                </td>
-                                <td className="trackname" key={track.name}>{track.name}</td>
-                                <td className="maxalt" key={track.name + "maxalt"}>{track.maxAltitude()}m</td>
-                                <td className="duration" key={track.name + "duration"}>{track.duration()}</td>
-                            </tr>
-                        )
-                    })
-                }
-                </tbody>
-            </table>
-        </div>
+            <TableContainer>
+                <Table size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell></TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Max Alt.</TableCell>
+                            <TableCell>Duration</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody> {
+                        props.tracks.map((track, i) => {
+                            return (
+                                <TableRow key={"tr" + i}>
+                                    <TableCell>
+                                        <Checkbox color="primary" checked={track.show} onChange={() => props.onChange(track.id)} />
+                                    </TableCell>
+                                    <TableCell key={"track-color-td" + i}>
+                                        <div className="track-color" key={"track-color" + i} style={{ backgroundColor: track.color.toCssHexString() }}>　</div>
+                                    </TableCell>
+                                    <TableCell className="trackname" key={track.name}>{track.name}</TableCell>
+                                    <TableCell className="maxalt" key={track.name + "maxalt"}>{track.maxAltitude()}m</TableCell>
+                                    <TableCell className="duration" key={track.name + "duration"}>{track.duration()}</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div >
     );
 };
