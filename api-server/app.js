7ce2b36dbbe4a84db97c7f5d18dac3541803367d
Keizaburo Takase <tkzrsf@gmail.com>
2023-12-17 00:56:51 +0900

    fetch track data from firestore and gcs

--- api-server/app.js
+++ api-server/app.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const { glob } = require('glob')
+const Firestore = require('@google-cloud/firestore')
+const { Storage } = require('@google-cloud/storage')
+
 const app = express()
-const port = 80
+const port = 3001
+
+const db = new Firestore({
+  projectId: 'wefly-407313',
+  keyFilename: './wefly-407313-855d6567493e.json',
+})
+const storage = new Storage();
+const bucketName = 'wefly';
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -9,19 +19,44 @@ app.get('/', (req, res) => {
 
 // list tracks
 app.get('/api/tracks/:date', (req, res) => {
-  glob(`tracks/${req.params.date}/*.json`).then(files => {
-    // add CORS header
-    res.header("Access-Control-Allow-Origin", "*")
-    res.send(files.map(file => {
-      return file.replace(`tracks/${req.params.date}/`, "")
-    }))
-  })
+  // parse date using dayjs
+  const startOfDay = new Date(req.params.date);
+  startOfDay.setHours(0, 0, 0, 0);
+  const endOfDay = new Date(req.params.date)
+  endOfDay.setHours(23, 59, 59, 999);
+  db.collection('tracks')
+    .where('lasttime', '>=', startOfDay)
+    .where('lasttime', '<', endOfDay).get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        res.status(404).send('No matching documents.');
+        return;
+      }
+      const tracks = new Array();
+      snapshot.forEach(doc => {
+        tracks.push(doc.data().trackid);
+      });
+      res.header("Access-Control-Allow-Origin", "*")
+      res.send(tracks);
+    });
 })
 
-app.get('/api/tracks/:date/:track', (req, res) => {
-  // add CORS header
-  res.header("Access-Control-Allow-Origin", "*")
-  res.sendFile(`${__dirname}/tracks/${req.params.date}/${req.params.track}`)
+app.get('/api/track/:trackid', (req, res) => {
+  const promise = db.collection('tracks')
+    .where('trackid', '==', req.params.trackid).get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        res.status(404).send('No matching documents.');
+        return;
+      }
+      const doc = snapshot.docs[0];
+      const basename = doc.data().file_url.split('/').pop();
+      storage.bucket(bucketName).file(basename).download().then(contents => {
+        contents = contents.toString();
+        res.header("Access-Control-Allow-Origin", "*")
+        res.send(contents);
+      });
+    });
 })
 
 app.listen(port, () => {
