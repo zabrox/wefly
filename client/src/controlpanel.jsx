d9f3664f1b12c27cbbb9e90e6a09f4ee552ebe84
Keizaburo Takase <tkzrsf@gmail.com>
2024-01-03 15:32:13 +0900

    fix cut off of tracklist

--- client/src/controlpanel.jsx
+++ client/src/controlpanel.jsx
@@ -29,7 +29,7 @@ export const ControlPanel = ({ date, onDateChange, tracks, onTrackClicked, contr
                         }} />
                 </center>
             </div>
-            <Box sx={{ width: '100%', height: '100%', overflow: 'auto', display: 'inline-flex' }}>
+            <Box id='tracklist-container'>
                 <TableContainer id='tracklist'>
                     <Table size="medium">
                         <TrackListHeader
