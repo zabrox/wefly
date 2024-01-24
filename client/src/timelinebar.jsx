0c2ac52f3b6a15dec15088a48f9fb149d8ca240c
Keizaburo Takase <tkzrsf@gmail.com>
2024-01-24 12:20:39 +0900

    handle mouse event on timelinebar-container

--- client/src/timelinebar.jsx
+++ client/src/timelinebar.jsx
@@ -35,11 +35,51 @@ const TimelineBar = ({ playbackState }) => {
 }
 
 export const TimelineBarContainer = ({ playbackState, setPlaybackState }) => {
+    const [isDragging, setIsDragging] = React.useState(false);
+
+    const handleClick = ((e) => {
+        document.getElementById('playlist-table').click();
+    });
+    const handleMouseDown = ((e) => {
+        setIsDragging(true);
+    });
+    const handleMouseUp = ((e) => {
+        setIsDragging(false);
+    });
+    const handleMove = React.useCallback((x, rect) => {
+        if (isDragging) {
+            const totalFlightDuration = Cesium.JulianDate.secondsDifference(
+                CesiumMap.viewer.clock.stopTime,
+                CesiumMap.viewer.clock.startTime);
+            const currentTime = dayjs(Cesium.JulianDate.toDate(Cesium.JulianDate.addSeconds(
+                CesiumMap.viewer.clock.startTime,
+                totalFlightDuration * x / rect.width,
+                new Cesium.JulianDate())));
+            CesiumMap.viewer.clock.currentTime = Cesium.JulianDate.fromIso8601(currentTime.format('YYYY-MM-DDTHH:mm:ssZ'));
+            setPlaybackState({ ...playbackState, currentTime: currentTime });
+        }
+    }, [playbackState]);
+    const handleMouseMove = (e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        handleMove(x, rect);
+    };
+    const handleTouchMove = (e) => {
+        const rect = document.getElementById('timelinebar-container').getBoundingClientRect();
+        const x = e.touches[0].clientX - rect.left;
+        handleMove(x, rect);
+    }
 
     return (
-        <div id='timelinebar-container'>
+        <div id='timelinebar-container'
+            onClick={handleClick}
+            onMouseUp={handleMouseUp}
+            onMouseLeave={handleMouseUp}
+            onTouchEnd={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            onTouchMove={handleTouchMove}>
             <TimelineBar playbackState={playbackState} />
-            <TimelineBarHandle playbackState={playbackState} setPlaybackState={setPlaybackState} />
+            <TimelineBarHandle playbackState={playbackState} onMouseDown={handleMouseDown} />
         </div>
     )
 }
