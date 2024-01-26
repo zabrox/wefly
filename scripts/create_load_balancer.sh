06f00437b2e095df9bbb2020ace76108a091cd24
Keizaburo Takase <tkzrsf@gmail.com>
2024-01-26 14:42:46 +0900

    add load balancer manage script

--- scripts/create_load_balancer.sh
+++ scripts/create_load_balancer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-gcloud compute network-endpoint-groups create wefly-api-server-neg --region=asia-northeast1 --network-endpoint-type=serverless --cloud-run-service=wefly-client
-gcloud compute network-endpoint-groups create wefly-client-neg --region=asia-northeast1 --network-endpoint-type=serverless --cloud-run-service=wefly-api-server
+gcloud compute network-endpoint-groups create wefly-api-server-neg --region=asia-northeast1 --network-endpoint-type=serverless --cloud-run-service=wefly-api-server
+gcloud compute network-endpoint-groups create wefly-client-neg --region=asia-northeast1 --network-endpoint-type=serverless --cloud-run-service=wefly-client
 gcloud compute backend-services create wefly-backend --global
 gcloud compute backend-services add-backend wefly-backend --global --network-endpoint-group=wefly-client-neg --network-endpoint-group-region=asia-northeast1
 gcloud compute backend-services create wefly-api-server-backend --global
 gcloud compute backend-services add-backend wefly-api-server-backend --global --network-endpoint-group=wefly-api-server-neg --network-endpoint-group-region=asia-northeast1
 gcloud compute url-maps create wefly-url-map --default-service=wefly-backend
-gcloud compute url-maps import wefly-url-map --global --source=../url-map.yml
+gcloud compute url-maps import wefly-url-map --global --source=../url-map.yml --quiet
 gcloud compute target-https-proxies create wefly-https-proxy \
     --ssl-certificates=wefly-certificate \
     --url-map=wefly-url-map
