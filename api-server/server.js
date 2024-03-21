6a12afeb895f583c32564c88a8a2c002d4d3607a
Keizaburo Takase <tkzrsf@gmail.com>
2024-03-22 02:52:00 +0900

    raise response size limit of api server

--- api-server/server.js
+++ api-server/server.js
@@ -6,7 +6,7 @@ const port = 8080;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '200mb' }));
 
 registerTracksEndpoint(app)
 registerPlacenameEndpoint(app)
