0753c414f2220f634949283580d3f5396f6da914
Keizaburo Takase <tkzrsf@gmail.com>
2024-04-02 01:06:53 +0900

    show error message when no tracks to playback

--- client/src/modes/scattermode/scatteractiondial.jsx
+++ client/src/modes/scattermode/scatteractiondial.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
-import { SpeedDial, SpeedDialAction } from '@mui/material';
+import { Box, SpeedDial, SpeedDialAction, Snackbar, Alert } from '@mui/material';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import { getTracksInPerspective, leaveScatterMode } from './scattermap';
 import * as Mode from '../mode';
 
 export const ScatterActionDial = ({ state, setState, scatterState }) => {
+    const [errorMessage, setErrorMessage] = React.useState('');
+
+    const handlePlayback = React.useCallback((tracks, errorMessage) => {
+        if (tracks.length === 0) {
+            setErrorMessage(errorMessage);
+            return;
+        }
+        leaveScatterMode();
+        setState({ ...state, mode: Mode.PLAYBACK_MODE, actionTargetTracks: tracks });
+    }, [state]);
+
+    const handlePlaybackSelection = React.useCallback(() => {
+        const selected = state.tracks.filter(track => scatterState.selectedTracks.has(track.getId()));
+        handlePlayback(selected, '再生するトラックを選択してください');
+    }, [state, scatterState.selectedTracks])
+
+    const handlePlaybackInPerspective = React.useCallback(() => {
+        const tracks = getTracksInPerspective(state.tracks);
+        handlePlayback(tracks, '視野内に再生可能なトラックがありません');
+    }, [state]);
+
     return (
-        <SpeedDial id='scatter-action-dial' ariaLabel='Scatter Mode Action Dial' size="medium" icon={<PlayArrowIcon />}
-            sx={{
-                position: 'absolute',
-                bottom: '50px',
-                left: state.controlPanelSize - 85,
-            }}>
-            <SpeedDialAction
-                key='選択中のトラックを再生'
-                icon={<PlayArrowIcon />}
-                tooltipTitle='選択中のトラックを再生'
-                tooltipOpen
-                onClick={React.useCallback(() => {
-                    leaveScatterMode();
-                    const selected = state.tracks.filter(track => scatterState.selectedTracks.has(track.getId()));
-                    setState({ ...state, mode: Mode.PLAYBACK_MODE, actionTargetTracks: selected });
-                }, [state, scatterState.selectedTracks])}
-            />,
-            <SpeedDialAction
-                key='視野内のトラックを再生'
-                icon={<VisibilityIcon />}
-                tooltipTitle='視野内のトラックを再生'
-                tooltipOpen
-                onClick={React.useCallback(() => {
-                    const tracks = getTracksInPerspective(state.tracks);
-                    leaveScatterMode();
-                    setState({ ...state, mode: Mode.PLAYBACK_MODE, actionTargetTracks: tracks });
-                }, [state])}
-            />
-        </SpeedDial>
+        <Box>
+            <SpeedDial id='scatter-action-dial' ariaLabel='Scatter Mode Action Dial' size="medium" icon={<PlayArrowIcon />}
+                sx={{
+                    position: 'absolute',
+                    bottom: '50px',
+                    left: state.controlPanelSize - 85,
+                }}>
+                <SpeedDialAction
+                    key='選択中のトラックを再生'
+                    icon={<PlayArrowIcon />}
+                    tooltipTitle='選択中のトラックを再生'
+                    tooltipOpen
+                    onClick={handlePlaybackSelection}
+                />,
+                <SpeedDialAction
+                    key='視野内のトラックを再生'
+                    icon={<VisibilityIcon />}
+                    tooltipTitle='視野内のトラックを再生'
+                    tooltipOpen
+                    onClick={handlePlaybackInPerspective}
+                />
+            </SpeedDial>
+            <Snackbar open={errorMessage !== ''}
+                onClose={() => setErrorMessage('')}
+                autoHideDuration={3000}>
+                <Alert severity='error'>{errorMessage}</Alert>
+            </Snackbar>
+        </Box>
     );
 }
