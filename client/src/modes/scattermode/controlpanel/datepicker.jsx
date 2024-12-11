dfee8eded321bf2c1bc813ccdfbb32f2b9619bf9
Keizaburo Takase <tkzrsf@gmail.com>
2024-12-11 23:43:18 +0900

    adjust today icon position

--- client/src/modes/scattermode/controlpanel/datepicker.jsx
+++ client/src/modes/scattermode/controlpanel/datepicker.jsx
@@ -1,11 +1,12 @@
 import { Box } from '@mui/system';
 import UpcomingIcon from '@mui/icons-material/Upcoming';
 import { MobileDatePicker } from '@mui/x-date-pickers';
+import './datepicker.css';
 
 export const DatePicker = ({ searchCondition, handleDateChange, handleTodayIconClick }) => {
     return (
         <Box id='date-picker-container'>
-            <MobileDatePicker
+            <MobileDatePicker id='mobile-date-picker'
                 value={searchCondition.from}
                 format="YYYY-MM-DD (ddd)"
                 onAccept={handleDateChange} />
