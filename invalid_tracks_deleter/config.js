e86a4f249c6e72efc106fbdeb7258cb196e7a8ca
Keizaburo Takase <tkzrsf@gmail.com>
2025-02-06 01:39:07 +0900

    API_URLの修正

--- invalid_tracks_deleter/config.js
+++ invalid_tracks_deleter/config.js
@@ -1,4 +1,4 @@
-// const API_URL = 'https://www.wefly.tokyo/api';
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'https://www.wefly.tokyo/api';
+// const API_URL = 'http://localhost:8080/api';
 
 module.exports = { API_URL };
